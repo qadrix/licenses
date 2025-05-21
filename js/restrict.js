3f583fdfb07b1095b87a3175f3d13a15ac81aa43
QADRIX <172352760+qadrix@users.noreply.github.com>
2025-05-20 22:43:51 -0400

    Update restrict.js

--- js/restrict.js
+++ js/restrict.js
@@ -1,5 +1,23 @@
 // Restriction of origin
 
-if (window.self !== window.top) {
-  window.location.href = './../404.html'; // Redirect if inside an iframe
+// Helper function that redirects the embedded app to my site.
+function redirectToMySite() {
+  window.location.href = "https://www.qadrix.com";
 }
+
+function guardEmbedding() {
+  const topLocation = window.top?.location;
+
+  // Same-origin policy
+  try {
+    topLocation.hostname;
+  } catch (e) {
+    if (e instanceof DOMException) {
+      console.error("Access to this app from an unknown host is prohibited.");
+      redirectToMySite();
+      return;
+    }
+  }
+}
+
+guardEmbedding();
