7fdd3f72e4f125a5c4cdcacb49aa7dbf3fd92399
QADRIX <172352760+qadrix@users.noreply.github.com>
2025-05-20 22:15:52 -0400

    Update restrict.js

--- js/restrict.js
+++ js/restrict.js
@@ -5,6 +5,7 @@ const url = new URL(window.location.href);
 
 if (url.origin !== currentOrigin) {
     console.log("Access blocked: URL is not from the same origin.");
+    window.location.href("about:blank");
     // Optionally, you can prevent further actions or redirect the user
 } else {
     console.log("Access allowed: URL is from the same origin.");
