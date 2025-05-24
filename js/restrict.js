87a8a1d189fb105f0ee5b28c1d5f010e8c7f2fc4
QADRIX <172352760+qadrix@users.noreply.github.com>
2025-05-23 22:02:06 -0400

    Update restrict.js

--- js/restrict.js
+++ js/restrict.js
@@ -4,7 +4,6 @@ if (window === window.top) {
 } else {
     // Clear the entire page and show a 403 Forbidden message
     document.body.innerHTML = '';
-    document.open();
     const forbiddenMessage = document.createElement('h1');
     forbiddenMessage.textContent = '403 Forbidden';
     forbiddenMessage.style.textAlign = 'center';
