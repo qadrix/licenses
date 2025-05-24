3b8a10f3dbcf2179ec8156b2b21826f773c4fa6d
QADRIX <172352760+qadrix@users.noreply.github.com>
2025-05-23 21:47:55 -0400

    Update restrict.js

--- js/restrict.js
+++ js/restrict.js
@@ -1,12 +1,12 @@
 // Restriction of origin
-    if (window === window.top) {
-        // Do nothing, page is from the origin
-    } else {
-        // Clear the entire page and show a 403 Forbidden message
-        document.body.innerHTML = '';
-        const forbiddenMessage = document.createElement('h1');
-        forbiddenMessage.textContent = '403 Forbidden';
-        forbiddenMessage.style.textAlign = 'center';
-        forbiddenMessage.style.marginTop = '20vh';
-        document.body.appendChild(forbiddenMessage);
-    }
+if (window === window.top) {
+    // Do nothing, page is from the origin
+} else {
+    // Clear the entire page and show a 403 Forbidden message
+    document.body.innerHTML = '';
+    const forbiddenMessage = document.createElement('h1');
+    forbiddenMessage.textContent = '403 Forbidden';
+    forbiddenMessage.style.textAlign = 'center';
+    forbiddenMessage.style.marginTop = '20vh';
+    document.body.appendChild(forbiddenMessage);
+}
