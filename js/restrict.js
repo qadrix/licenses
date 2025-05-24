fd363ed20b3c1cae3c915dad42492ba20d2e5eb0
QADRIX <172352760+qadrix@users.noreply.github.com>
2025-05-23 21:52:24 -0400

    Update restrict.js

--- js/restrict.js
+++ js/restrict.js
@@ -3,7 +3,8 @@ if (window === window.top) {
     // Do nothing, page is from the origin
 } else {
     // Clear the entire page and show a 403 Forbidden message
-    document.innerHTML = '';
+    document.head.innerHTML = '';
+    document.body.innerHTML = '';
     const forbiddenMessage = document.createElement('h1');
     forbiddenMessage.textContent = '403 Forbidden';
     forbiddenMessage.style.textAlign = 'center';
