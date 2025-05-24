d32936c885b10582fd4beea456ba1a9d9c51c168
QADRIX <172352760+qadrix@users.noreply.github.com>
2025-05-23 21:21:51 -0400

    Update restrict.js

--- js/restrict.js
+++ js/restrict.js
@@ -1,3 +1,15 @@
 // Restriction of origin
-const div = document.querySelector('a');
-div.classList.remove('logo');
+function checkOrigin() {
+    if (window === window.top) {
+        console.log("Verified");
+        // You can also update a DOM element here
+        document.getElementById("originStatus").innerText = "Verified";
+    } else {
+        console.log("3rd party embed");
+        // Update a DOM element
+        document.getElementById("originStatus").innerText = "3rd party embed";
+    }
+}
+
+// Call the function
+checkOrigin();
