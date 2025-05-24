73ab0bcd4975718d2f958fa575d1cfa51d32aa80
QADRIX <172352760+qadrix@users.noreply.github.com>
2025-05-23 21:13:05 -0400

    Update restrict.js

--- js/restrict.js
+++ js/restrict.js
@@ -1,7 +1,9 @@
 // Restriction of origin
-if (window.location.href !== 'https://qadrix.github.io/licenses/') {
+var currentUrl = window.location.href;
+
+if (currentUrl !== 'https://qadrix.github.io/licenses/') {
   // If the URL is not authorized, prevent the page from loading
   document.write = function() {};
-  document.body.innerHTML = 'test';
+  document.body.innerHTML = '';
   throw new Error('This page cannot be accessed directly');
 }
